02014404ccc76af1ad1b18a95ddd57fe488d8c39
Matthieu Prat <matthieuprat@gmail.com>
2016-02-04 23:30:29 +0100

    Refactor

--- timer.dist.js
+++ timer.dist.js
@@ -1,41 +1,95 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Timer = function () {
+  function Timer(t, cb) {
+    _classCallCheck(this, Timer);
+
+    this.reset(t);
+    this.cb = cb;
+  }
+
+  _createClass(Timer, [{
+    key: 'start',
+    value: function start() {
+      var _this = this;
+
+      if (this.tid !== undefined) return;
+      this.tid = setInterval(function () {
+        _this.cb(_this);
+        if (_this.time() < 0) _this.stop();
+      }, 1000);
+    }
+  }, {
+    key: 'stop',
+    value: function stop() {
+      clearInterval(this.tid);
+      this.tid = undefined;
+    }
+  }, {
+    key: 'reset',
+    value: function reset() {
+      var t = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+
+      this.to = Timer.now(t);
+    }
+  }, {
+    key: 'time',
+    value: function time() {
+      return this.to - Timer.now();
+    }
+  }, {
+    key: 'toString',
+    value: function toString() {
+      var t = this.time();
+      if (t <= 0) return '00';
+      return [t / 3600, t / 60 % 60, t % 60].map(function (t) {
+        return 0 | t;
+      }).reduce(function (a, t) {
+        return (a.length || t) && a.push(t), a;
+      }, []).map(function (t) {
+        return ('0' + t).slice(-2);
+      }).join(':');
+    }
+  }], [{
+    key: 'parse',
+    value: function parse(s) {
+      var components = s.split(':').map(function (v) {
+        return parseInt(v, 10);
+      });
+      return components.reduceRight(function (t, c, i) {
+        return t + Math.pow(60, components.length - i - 1) * c;
+      });
+    }
+  }, {
+    key: 'now',
+    value: function now() {
+      var offset = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+
+      return 0 | Date.now() / 1000 + offset;
+    }
+  }]);
+
+  return Timer;
+}();
+
 var p = function p(_p) {
   return decodeURI((new RegExp('[#&]' + _p + '=([^&]*)').exec(window.location.hash) || [])[1] || '');
 };
-var e = document.getElementsByTagName('input')[0];
-var tid = undefined;
-var to = undefined;
-var time = function time(t) {
-  return 0 | Date.now() / 1000 + (t || 0);
-};
-var displayTimer = function displayTimer() {
-  var t = Math.max(to - time(), 0);
-  e.value = [t / 3600, t / 60 % 60, t % 60].map(function (t) {
-    return 0 | t;
-  }).reduce(function (a, t) {
-    return (a.length || t) && a.push(t), a;
-  }, []).map(function (t) {
-    return ('0' + t).slice(-2);
-  }).join(':') || '00';
-};
-var startTimer = function startTimer() {
-  displayTimer();
-  tid = setInterval(function () {
-    if (to < time()) clearInterval(tid);
-    displayTimer();
-  }, 1000);
-};
-e.addEventListener('focus', function () {
-  clearInterval(tid);
+var elt = document.getElementsByTagName('input')[0];
+var timer = new Timer(Timer.parse(p('t') || '3600'), function (t) {
+  elt.value = t.toString();
 });
-e.addEventListener('blur', function () {
-  var t = e.value.split(':').map(function (v) {
-    return parseInt(v, 10);
-  });
-  t = (t.pop() || 0) + (t.pop() || 0) * 60 + (t.pop() || 0) * 3600;
-  to = time(t);
-  startTimer();
+
+elt.addEventListener('focus', function () {
+  return timer.stop();
 });
-to = time(parseInt(p('t'), 10) || 3600);
-startTimer();
+elt.addEventListener('blur', function () {
+  timer.reset(Timer.parse(elt.value));
+  timer.start();
+});
+elt.value = timer.toString();
+timer.start();
--- timer.js
+++ timer.js
@@ -1,31 +1,51 @@
-let p = p => decodeURI((new RegExp(`[#&]${p}=([^&]*)`).exec(window.location.hash) || [])[1] || '')
-let e = document.getElementsByTagName('input')[0]
-let tid
-let to
-let time = t => 0 | Date.now() / 1000 + (t || 0)
-let displayTimer = () => {
-  let t = Math.max(to - time(), 0)
-  e.value = [t / 3600, t / 60 % 60, t % 60]
-    .map(t => 0 | t)
-    .reduce((a, t) => ((a.length || t) && a.push(t), a), [])
-    .map(t => ('0' + t).slice(-2))
-    .join(':') || '00'
+class Timer {
+  constructor (t, cb) {
+    this.reset(t)
+    this.cb = cb
+  }
+  start () {
+    if (this.tid !== undefined) return
+    this.tid = setInterval(() => {
+      this.cb(this)
+      if (this.time() < 0) this.stop()
+    }, 1000)
+  }
+  stop () {
+    clearInterval(this.tid)
+    this.tid = undefined
+  }
+  reset (t = 0) {
+    this.to = Timer.now(t)
+  }
+  time () {
+    return this.to - Timer.now()
+  }
+  toString () {
+    let t = this.time()
+    if (t <= 0) return '00'
+    return [t / 3600, t / 60 % 60, t % 60]
+           .map(t => 0 | t)
+           .reduce((a, t) => ((a.length || t) && a.push(t), a), [])
+           .map(t => ('0' + t).slice(-2))
+           .join(':')
+  }
+  static parse (s) {
+    let components = s.split(':').map(v => parseInt(v, 10))
+    return components.reduceRight((t, c, i) => t + Math.pow(60, components.length - i - 1) * c)
+  }
+  static now (offset = 0) {
+    return 0 | Date.now() / 1000 + offset
+  }
 }
-let startTimer = () => {
-  displayTimer()
-  tid = setInterval(() => {
-    if (to < time()) clearInterval(tid)
-    displayTimer()
-  }, 1000)
-}
-e.addEventListener('focus', () => {
-  clearInterval(tid)
+
+const p = p => decodeURI((new RegExp(`[#&]${p}=([^&]*)`).exec(window.location.hash) || [])[1] || '')
+const elt = document.getElementsByTagName('input')[0]
+const timer = new Timer(Timer.parse(p('t') || '3600'), (t) => { elt.value = t.toString() })
+
+elt.addEventListener('focus', () => timer.stop())
+elt.addEventListener('blur', () => {
+  timer.reset(Timer.parse(elt.value))
+  timer.start()
 })
-e.addEventListener('blur', () => {
-  let t = e.value.split(':').map(v => parseInt(v, 10))
-  t = (t.pop() || 0) + (t.pop() || 0) * 60 + (t.pop() || 0) * 3600
-  to = time(t)
-  startTimer()
-})
-to = time(parseInt(p('t'), 10) || 3600)
-startTimer()
+elt.value = timer.toString()
+timer.start()
